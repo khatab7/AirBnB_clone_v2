f4313fdf297cabded3e8d1cdee42f6f22819cd6d
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 <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>
2024-05-24 01:42:47 +0200

    task6

--- models/state.py
+++ models/state.py
@@ -1,8 +1,28 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('city', backref='state', cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """
+
+            """
+            import models
+            from models.city import City
+            City_list = []
+            for city in models.storage.all(city).values():
+                if city.state_id == self.id:
+                    City_list.append(city)
+            return City_list
