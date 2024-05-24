c2d7c80e4412555c48b4b9250297725847aff453
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 <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>
2024-05-24 21:25:04 +0200

    fix

--- models/state.py
+++ models/state.py
@@ -1,28 +1,35 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from os import getenv
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+"""This is the state class"""
 from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.orm import relationship, backref
+import models
+from os import environ
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
 
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        cities = relationship('city', backref='state', cascade='all, delete-orphan')
+    if environ.get('HBNB_TYPE_STORAGE') == "db":
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete, delete-orphan")
     else:
+
         @property
         def cities(self):
-            """
+            """ Returns the list of City instances with
+            state_id == current State.id """
+            all_cities = models.storage.all(City)
+            state_cities = []
+            for city_ins in all_cities.values():
+                if city_ins.state_id == self.id:
+                    state_cities.append(city_ins)
 
-            """
-            import models
-            from models.city import City
-            City_list = []
-            for city in models.storage.all(city).values():
-                if city.state_id == self.id:
-                    City_list.append(city)
-            return City_list
+            return state_cities
