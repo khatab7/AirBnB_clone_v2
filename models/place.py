ee3989fa57767d0dd8e82c2160312a01486c4337
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 <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>
2024-05-25 21:27:17 +0200

    add user city places

--- models/place.py
+++ models/place.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import (Column, String, 
-                        ForeignKey, Float, 
-                        Integer)
-from models.base_model import BaseModel, Base
+#8from sqlalchemy import (Column, String, 
+#8                        ForeignKey, Float, 
+#8                        Integer)
+from models.base_model import BaseModel #8, Base
 
 
-class Place(BaseModel, Base):
+class Place(BaseModel):#BASE8
     """ A place to stay """
 
     __tablename__ = 'places'
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024))
-    number_rooms = Column(Integer, default=0)
-    number_bathrooms = Column(Integer, default=0)
-    max_guest = Column(Integer, default=0)
-    price_by_night = Column(Integer, default=0)
-    latitude = Column(Float)
-    longitude = Column(Float)
+    city_id = ""#8Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = ""#8Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = ""#8Column(String(128), nullable=False)
+    description = ""#8Column(String(1024))
+    number_rooms = 0#8Column(Integer, default=0)
+    number_bathrooms = 0#8Column(Integer, default=0)
+    max_guest = 0#8Column(Integer, default=0)
+    price_by_night = 0#8Column(Integer, default=0)
+    latitude = 0.0#8Column(Float)
+    longitude = 0.0#8Column(Float)
     amenity_ids = []
