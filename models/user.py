67549a3f1d45ca07d15b09de0cc4fed45626325c
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 <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>
2024-05-26 21:26:52 +0200

    user fix

--- models/user.py
+++ models/user.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-#from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -10,6 +9,7 @@ class User(BaseModel, Base):
     __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    #places = relationship('Place', backref='user', cascade='all, delete-orphan')
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", cascade="all, delete", backref="user",
+                          passive_deletes=True)
