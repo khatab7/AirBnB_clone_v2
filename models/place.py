75ea65c7d369b71f2b50f2af698a3414242603be
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 <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>
2024-05-27 01:20:13 +0200

    add and fix amenity and places

--- models/place.py
+++ models/place.py
@@ -1,11 +1,22 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from models.review import Review
+from models.amenity import Amenity
 import models
 from os import getenv
+
+
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
 class Place(BaseModel, Base):
@@ -23,11 +34,14 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float)
     longitude = Column(Float)
-    reviews = relationship("Review", cascade="all, delete",
-                               backref="place", passive_deletes=True)
     amenity_ids = []
 
     if getenv('HBNB_TYPE_STORAGE') == 'db':
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="place", passive_deletes=True)
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False, backref='place_amenities')
+    else:
         @property
         def reviews(self):
             """Return the list of reviews"""
@@ -36,3 +50,19 @@ class Place(BaseModel, Base):
                 if value.place_id == self.id:
                     list_reviews.append(value)
             return list_reviews
+
+        @property
+        def amenities(self):
+            """Return the list of amenities"""
+            list_amenities = []
+            for value in models.storage.all(Amenity).values():
+                if value.place_id == self.id:
+                    list_amenities.append(value)
+            return list_amenities
+
+        @amenities.setter
+        def amenities(self, cls):
+            """Add the id of an amenity"""
+            if not isinstance(cls, Amenity):
+                return
+            self.amenity_ids.append(cls.id)
