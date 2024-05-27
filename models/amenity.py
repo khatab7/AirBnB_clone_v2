668577cd20b22eacc0ff93d858e99b4566b84de5
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 <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>
2024-05-27 02:18:03 +0200

    add and fix amenity and places

--- models/amenity.py
+++ models/amenity.py
@@ -10,3 +10,4 @@ class Amenity(BaseModel, Base):
 
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
+    place_amenity = relationship('Place', secondary='place_amenity', viewonly=True)
