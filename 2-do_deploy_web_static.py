ce6a5763be7cd8f387a80a8f0a3c5e029c2a4c53
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 <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>
2024-07-27 15:31:36 -0700

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,29 +1,55 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+"""Fabric Configuration script for deploying web_static"""
+from fabric.operations import local
+from fabric.operations import put
+from fabric.operations import run
+from fabric.api import env
+from fabric.api import hosts
+from datetime import datetime
+from fabric.api import runs_once
+import os
 
-from fabric.api import put, run, env
-from os.path import exists
 env.hosts = ['100.24.74.180', '52.86.119.1']
 
+@runs_once
+def do_pack():
+    """Pack web_static folder for future deployment"""
+    filename = "versions/web_static_{}.tgz".format(
+        datetime.now().strftime("%Y%m%d%H%M%S"))
+    local("mkdir -p versions")
+    result = local("tar -czvf {} web_static".format(filename))
+    if result.failed is True:
+        return None
+    else:
+        return filename
+
+
 def do_deploy(archive_path):
-    """distributes an archive to the web servers"""
-    if exists(archive_path) is False:
+    """Deploy given archive to web server"""
+    if os.path.exists(archive_path) is False:
         return False
-    try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
-        put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-        return True
-    except:
+    filename = os.path.basename(archive_path)
+    result = put(archive_path, '/tmp/{}'.format(filename))
+    if result.failed is True:
         return False
+    result = run("mkdir -p /data/web_static/releases/{}/"
+                 .format(filename[:-4]))
+    if result.failed is True:
+        return False
+    result = run(("tar -xzf /tmp/" +
+                  "{} -C /data/web_static/releases/{}/" +
+                  " --strip-components=1").format(filename, filename[:-4]))
+    if result.failed is True:
+        return False
+    result = run("rm /tmp/{}".format(filename))
+    if result.failed is True:
+        return False
+    result = run("rm -rf /data/web_static/current")
+    if result.failed is True:
+        return False
+    result = run("ln -s /data/web_static/releases/"
+                 "{}/ /data/web_static/current".format(filename[:-4]))
+    if result.failed is True:
+        return False
+    print("New version deployed!")
+    return True
